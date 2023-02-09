ed25646fb0ad35a56c8526440d151b57e66d9cc6
aurilyn <caobenny122@gmail.com>
2023-02-09 15:57:38 -0800

    added some static values and comments, adjusted default foils to 470 instead of 0

--- new files/input.py
+++ new files/input.py
@@ -1,17 +1,29 @@
-def get_foil_information()
-    foil = 
-    {
-        foil_quantity : 0
-        foil_shape : 'd-shape'
-        etc : 'etc'
-    }
+def get_foil_information():
+    foil = {
+            "foil_quantity": 470,
+            "foil_shape": 'd-shape',
+            "target-file": "filelocation",
+            "temp": 2300,
+            "foil-height": 0.525,
+            "thickness": 25,
+            "foil-rotation": 0,
+            "ionizer": 5.956
+            # etc: 'etc'
+        }
     ###This container should contain all variable information (Maybe static information as well, easier to generate here?) of a foil.
     ### we will use this function data to create the foils by calling it in main, and sending it to static_objects.py
-    foil.foil_quantity = input("How many foils would like to make? default is 0: ")
+    foil.foil_quantity = input("How many foils would like to make? default is 470: ")
     return foil
 
-def get_anything_else()
+def get_anything_else(foil):
     ### Do we need anything else?
+    if foil.foil_shape != "d-shape":
+        print("foil shape list")
+        foil.foil_shape = input("What shape from above would you like to use?")
+        # adjust the dictionary according to the shape that is selected, may need to
+        # add or remove key/value from dictionary as some won't be used
+    ## if statements to check foil shape under here probably
+    return foil
     pass
 
 
